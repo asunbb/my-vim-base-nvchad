88c50d3727a0dd0fdbe950e72e4e24844b157769
asunbb <asunbb@qq.com>
2025-10-13 15:37:40 +0800

    neovim，neovide 特殊配置，变更字体大小

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,18 @@
 require "nvchad.mappings"
 
--- add yours here
+---@type function
+local keymap = vim.keymap.set
 
-local map = vim.keymap.set
-
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+keymap("n", ";", ":", { desc = "CMD enter command mode" })
+keymap("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- 是否 neovim 运行在 neovide 中
+---@type boolean|nil
+local is_neovide = vim.g.neovide
+if is_neovide then
+  keymap({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+  keymap({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+end
+
--- lua/options.lua
+++ lua/options.lua
@@ -12,3 +12,11 @@ opt.cursorlineopt = "both"
 -- 显示光标相对行数
 opt.relativenumber = true
 
+-- 是否 neovim 运行在 neovide 中
+---@type boolean|nil
+local is_neovide = vim.g.neovide
+if is_neovide then
+  -- 设置字体和大小
+  o.guifont = "Hack Nerd Font Mono:h15"
+end
+
