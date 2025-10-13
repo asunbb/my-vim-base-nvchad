bd6836f2246708635ad0120aee23c452b26fc89d
asunbb <asunbb@qq.com>
2025-10-13 14:35:02 +0800

    neovim，配置更新，自定义

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,14 @@
+
 require "nvchad.options"
 
--- add yours here!
+-- asunbb 自定义
+-- 使用 :h vim.o 查看 vim.o 和 vim.opt 不同格式用法
+local o = vim.o
+local opt = vim.opt
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- 高亮光标行和光标行号
+opt.cursorlineopt = "both"
+
+-- 显示光标相对行数
+opt.relativenumber = true
+
