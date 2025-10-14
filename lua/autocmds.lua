ae14d1ddc5cfae3e4d5b610c499d511c20b2b890
asunbb <asunbb@qq.com>
2025-10-14 21:00:35 +0800

    neovim，自动命令，新增进入 buffer 时检查文件是否在 neovim 之后被修改，否则加载新文件

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,14 @@
 require "nvchad.autocmds"
+
+local creat_autocmd = vim.api.nvim_create_autocmd
+
+-- 进入 buffer 页面时自动检查刷新文件是否有更新
+creat_autocmd("BufEnter", {
+  ---@param args vim.api.keyset.create_autocmd.callback_args
+  callback = function(args)
+    vim.cmd(":checktime")
+  end,
+  -- 任意 buffer
+  pattern = "*",
+})
+
