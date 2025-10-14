4bca49f54d5f9e75894d8877dc64d9d2c6cf62db
asunbb <asunbb@qq.com>
2025-10-14 21:03:54 +0800

    一些注释

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,6 +3,7 @@ require "nvchad.autocmds"
 local creat_autocmd = vim.api.nvim_create_autocmd
 
 -- 进入 buffer 页面时自动检查刷新文件是否有更新
+-- autocmd 事件可参考 https://neovim.io/doc/user/autocmd.html#_5.-events
 creat_autocmd("BufEnter", {
   ---@param args vim.api.keyset.create_autocmd.callback_args
   callback = function(args)
