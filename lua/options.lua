9d6fed9f5c8fa2c99c57a1451226225c9a03f313
asunbb <asunbb@qq.com>
2025-10-15 16:00:37 +0800

    neovim，配置改进，自动格式化

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,7 @@
 
 require "nvchad.options"
 
--- asunbb 自定义
+-- ======================== asunbb 自定义
 -- 使用 :h vim.o 查看 vim.o 和 vim.opt 不同格式用法
 local o = vim.o
 local opt = vim.opt
@@ -19,4 +19,13 @@ if is_neovide then
   -- 设置字体和大小
   o.guifont = "Hack Nerd Font Mono:h15"
 end
+
+-- tab
+-- 每个 tab 的可见空格数量
+opt.tabstop = 4
+opt.softtabstop = 4
+-- 一个 tab 按键插入的空格数量
+opt.shiftwidth = 4
+-- 把 tab 按键扩展成空格
+opt.expandtab = true
 
